4d96ae8dab9898fa8828208c15ead35b2b644742
Anton Krichevskii <anton.krichevskii@similarweb.com>
2018-11-11 16:45:43 +0200

    chore(): Add editorconfig and env

--- bot/index.js
+++ bot/index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
+
 const TelegramBot = require('node-telegram-bot-api');
 
 // replace the value below with the Telegram token you receive from @BotFather
-const token = '756808701:AAFzUHISqXLkEoFluuyDIJN_FFoIu7o5RKw';
+const token = process.env.BOT_TOKEN;
 
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
