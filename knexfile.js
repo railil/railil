cb4620e5f8593d7adb1ffef062dbcae83c404ab5
Artem Maksimenko <artem.maksimenko@similarweb.com>
2018-12-03 12:06:19 +0200

    hotfix(): DB + migration

--- knexfile.js
+++ knexfile.js
@@ -1,28 +1,29 @@
 require('dotenv').config();
 module.exports = {
 
-  development: {
-    client: 'postgresql',
-    connection: process.env.DB_CONNECTION_DEVELOPMENT,
-    pool: {
-      min: 2,
-      max: 10
+    development: {
+        client: 'postgresql',
+        connection: process.env.DB_CONNECTION_DEVELOPMENT,
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: 'knex_migrations'
+        }
     },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
 
-  production: {
-    client: 'postgresql',
-    connection: process.env.DB_CONNECTION_DEVELOPMENT,
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+    production: {
+        client: 'postgresql',
+        connection: process.env.DB_CONNECTION_PRODUCTION,
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            directory: '/var/www/railil/migrations',
+            tableName: 'knex_migrations'
+        }
     }
-  }
 
 };
